ecfd043c8b9b7c4e939dee5e3402d0d222206956
Fayed Gaya <gayafayed@gmail.com>
2024-04-28 16:31:37 -0600

    Initial commit

---
+++ src/main/kotlin/com/fayed/fosi/FoSiApplication.kt
@@ -0,0 +1,11 @@
+package com.fayed.fosi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoSiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoSiApplication>(*args)
+}
---
+++ src/test/kotlin/com/fayed/fosi/FoSiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fayed.fosi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoSiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
