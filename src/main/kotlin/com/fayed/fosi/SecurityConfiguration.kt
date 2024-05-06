107182ecb001739014751a6f7c6e69c622f45f9f
Fayed Gaya <gayafayed@gmail.com>
2024-05-06 02:45:53 -0600

    Disable spring security

---
+++ src/main/kotlin/com/fayed/fosi/SecurityConfiguration.kt
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+//import org.springframework.security.config.annotation.web.invoke
+//import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+//@EnableWebSecurity
+class SecurityConfiguration {
+//    @Bean
+//    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+//        http {
+//            authorizeHttpRequests {
+//                authorize(anyRequest, permitAll)
+//            }
+//            formLogin { }
+//            httpBasic { }
+//        }
+//        return http.build()
+//    }
+}
